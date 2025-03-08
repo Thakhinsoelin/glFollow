050ade860525d83fec2daa2baaa3364b8a436990
soethiha <usersoe123@gmail.com>
2025-03-08 15:17:24 +0630

    Removed the build folder

--- build/CMakeFiles/fc-tmp/glfw/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for glfw")
-
-block(SCOPE_FOR VARIABLES)
-
-include("D:/Dev/c++/glFollow/build/CMakeFiles/fc-tmp/glfw/glfw-gitupdate.cmake")
-
-endblock()
