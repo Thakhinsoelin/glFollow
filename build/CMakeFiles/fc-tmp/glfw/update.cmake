5e76c9c0a0c9ac58b511d7a7d0e2641769667858
soethiha <usersoe123@gmail.com>
2025-03-08 15:11:06 +0630

    Add project files.

---
+++ build/CMakeFiles/fc-tmp/glfw/update.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing update step for glfw")
+
+block(SCOPE_FOR VARIABLES)
+
+include("D:/Dev/c++/glFollow/build/CMakeFiles/fc-tmp/glfw/glfw-gitupdate.cmake")
+
+endblock()
